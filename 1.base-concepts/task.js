7508925f6613185d261288b211630da9ee8d8cfd
Екатерина <laufeyconyy@gmail.com>
2021-12-10 13:18:46 +0300

    first homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d;
+  d = b ** 2 - (4 * a * c);
+  console.log(d);
+
+  if (d === 0) {
+    let x = (- b) / (2 * a);
+    arr.push(x);
+  } else if (d > 0) {
+    let x1 = (- b + Math.sqrt(d)) / (2 * a);
+    let x2 = (- b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+  } 
   return arr; // array
 }
 
