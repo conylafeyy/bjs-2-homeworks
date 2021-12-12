c41e498208c034ac734a74813bcf3ec7681d3b48
Екатерина <laufeyconyy@gmail.com>
2021-12-12 14:03:25 +0300

    second task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,63 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  
+  for (item of arr) {
+    if (min > item) {
+      min = item;
+    }
 
-  // Ваш код
+    if (max < item) {
+      max = item;
+    }
+
+    sum += item;
+  }
+  
+  let avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (item of arr) {
+    sum += item
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (arr of arrOfArr) {
+    if (max < func(arr)) {
+      max = func(arr);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+  
+  for (item of arr) {
+    if (min > item) {
+      min = item;
+    }
+
+    if (max < item) {
+      max = item;
+    }
+  }
+
+  let dist = Math.abs(max - min);
+  
+  return dist;
 }
