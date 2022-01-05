3933cec62206ec4f4117d82434dc2d69ffff1eec
Екатерина <laufeyconyy@gmail.com>
2022-01-05 11:28:39 +0300

    3 task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = (arr1.length === arr2.length && arr1.every(comp));
 
-  // Ваш код
-
+  function comp(item, index) {
+    return item === arr2[index];
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
   return resultArr; // array
 }
