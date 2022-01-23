c941a67b083357e83151f6132c528ff59e2ed35a
Ekaterina <58141966+conylafeyy@users.noreply.github.com>
2022-01-23 18:40:12 +0300

    8 task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,57 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    let hash = args.toString();
+    let idx = cache.findIndex((item) => item.hash === hash);
+
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    }
+
+    let result = func.call(this, ...args);
+    cache.push({ hash: hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(f, ms) {
+  let timeout;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function (...args) {
+    if (!timeout) {
+      f.apply(this, args);
+    }
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      f.apply(this, args);
+    }, ms);
+  };
+
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(f) {
+  let timeout;
+
+  function wrapper(...args) {
+    wrapper.count += 1;
+    if (!timeout) {
+      f.apply(this, args);
+    }
+    clearTimeout(timeout);
+    
+    timeout = setTimeout(() => {
+      f.apply(this, args);
+    }, ms);
+  };
+
+  wrapper.count = 0;
+  return wrapper
 }
